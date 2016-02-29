abd2a6b5945eeb53f49e2d4bc55cfe099ddde82c
Cameron Hunter <chunter@twitter.com>
2016-02-28 16:30:18 -0800

    Export all and any separately

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-export * from './combinators';
+export { default as all } from './all';
+export { default as any } from './any';
 export { default as conditional } from './conditional';
 export { default as delegate } from './delegate';
 export { default as event } from './event';
